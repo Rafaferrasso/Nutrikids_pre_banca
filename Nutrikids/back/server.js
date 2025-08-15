c14ed8eed38c71f7bf92c2587a10012ab0d5e01d
Rafaela Cardoso Ferrasso da Silva <rafaelferrassodasilva@MacBook-Air-de-Rafaela.local>
2025-08-15 20:27:54 -0300

    alteracao no server

--- Nutrikids/back/server.js
+++ Nutrikids/back/server.js
@@ -8,7 +8,6 @@ const { pool, testConnection, initTables } = require('./js/db');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middlewares
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
